e116a110786ffbbd6687e1f9030214ded23c5695
Shivesh Tiwari <stshandilya0820@gmail.com>
2021-05-20 11:22:53 +0530

    🚨 Warning fixed

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import {Route, BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CollegeApp from './College-Website/Index'
 
 function App() {
   return (
     <Router>
-          <switch>
+          <Switch>
             <Route path="/college" component={CollegeApp} />
-          </switch> 
+          </Switch> 
     </Router>
   );
 }
