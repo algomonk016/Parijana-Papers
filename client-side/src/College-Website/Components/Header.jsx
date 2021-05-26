334f0a8de03af15fd6efd4fcaae1d714ae4d758b
Shivesh Tiwari <stshandilya0820@gmail.com>
2021-05-26 19:22:41 +0530

    🔥 Replaced GIT by UIET Coding Club

--- client-side/src/College-Website/Components/Header.jsx
+++ client-side/src/College-Website/Components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import NavbarBrand from './NavbarBrand';
 import {faBookOpen, faPager, faAddressCard} from '@fortawesome/free-solid-svg-icons'
+import { faDiscord } from '@fortawesome/free-brands-svg-icons'
 import NavItem from './NavItem'
 import brandPic from '../Assests/Pics/army.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function Header() {
     return (
         <header className="text-gray-600 body-font shadow-md mb-3">
@@ -12,9 +15,9 @@ function Header() {
                     <NavItem icon = {faPager} color='indigo' item='Papers' directTo='/papers'/>
                     <NavItem icon = {faBookOpen} color='red' item='Notes' directTo='/notes' />
                     <NavItem icon = {faAddressCard} item='Support' directTo='/support' />
-                    <a href="https://github.com/algomonk016/Parijana-Papers" className="shaking inline-flex text-l items-center bg-gray-200 border-0 py-2 px-3 hover:bg-gray-300 rounded-md text-base mt-1">
-                        <svg className="mx-1 fill-current text-purple-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
-                        <span className="mx-1  tracking-wide">Git Repo</span>
+                    <a href="https://discord.gg/X4pUGEYJ5r" className="shaking inline-flex items-center bg-purple-200 border-0 py-1 px-2 text-purple-600 hover:text-purple-700 rounded-md text-base mt-1">
+                        <span className="text-lg mr-1"><FontAwesomeIcon icon={faDiscord} /></span>
+                        <span className="mx-1 tracking-wide">Join UIET Coding Club</span>
                     </a>
                 </nav>
             </div>
