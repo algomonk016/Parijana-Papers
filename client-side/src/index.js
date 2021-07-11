a8987d3a99d607551b795abfc34f80aeec4e53e6
algomonk016 <stshandilya0820@gmail.com>
2021-07-12 03:38:59 +0530

    ⚡️ Replaced Tailwind CDN with Package

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios'
+import './index.css'
 axios.defaults.baseURL = 'http://localhost:5000'
 ReactDOM.render(
   <>
